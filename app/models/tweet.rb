98ec03c46c4ec9e0a74b915046026a8d03163885
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-10-25 14:51:59 +0900

    search アクションの設定とビューの作成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,13 @@
 class Tweet < ApplicationRecord
     validates :text, presence: true
 
+    def self.search(search)
+        if search != ""
+            Tweet.where('text LIKE(?)', "%#{search}%")
+        else
+            Tweet.all
+        end
+    end
 
     belongs_to :user
     has_many :comments
